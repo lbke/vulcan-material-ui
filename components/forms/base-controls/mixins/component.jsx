73814a43f0228304cd9da0780c6bc158bee105e6
Eric Burel <e.burel.contact@gmail.com>
2018-10-29 10:45:19 +0100

    ignore formComponents

--- components/forms/base-controls/mixins/component.jsx
+++ components/forms/base-controls/mixins/component.jsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FormattedMessage } from 'meteor/vulcan:i18n';
-import _omit from 'lodash/omit';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { FormattedMessage } from "meteor/vulcan:i18n";
+import _omit from "lodash/omit";
 
 export default {
-  
   propTypes: {
     label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
     hideLabel: PropTypes.bool,
     layout: PropTypes.string,
     required: PropTypes.bool,
-    errors: PropTypes.arrayOf(PropTypes.object),
+    errors: PropTypes.arrayOf(PropTypes.object)
   },
-  
-  getFormControlProperties: function () {
+
+  getFormControlProperties: function() {
     return {
       label: this.props.label,
       hideLabel: this.props.hideLabel,
       layout: this.props.layout,
       required: this.props.required,
       hasErrors: this.hasErrors(),
-      className: this.props.className,
+      className: this.props.className
     };
   },
-  
-  getFormHelperProperties: function () {
+
+  getFormHelperProperties: function() {
     return {
       help: this.props.help,
       errors: this.props.errors,
@@ -33,18 +31,18 @@ export default {
       showCharsRemaining: this.props.showCharsRemaining,
       charsRemaining: this.props.charsRemaining,
       charsCount: this.props.charsCount,
-      max: this.props.max,
+      max: this.props.max
     };
   },
-  
-  hashString: function (string) {
+
+  hashString: function(string) {
     let hash = 0;
     for (let i = 0; i < string.length; i++) {
-      hash = (((hash << 5) - hash) + string.charCodeAt(i)) & 0xFFFFFFFF;
+      hash = ((hash << 5) - hash + string.charCodeAt(i)) & 0xffffffff;
     }
     return hash;
   },
-  
+
   /**
    * The ID is used as an attribute on the form control, and is used to allow
    * associating the label element with the form control.
@@ -52,82 +50,82 @@ export default {
    * If we don't explicitly pass an `id` prop, we generate one based on the
    * `name` and `label` properties.
    */
-  getId: function () {
+  getId: function() {
     if (this.props.id) {
       return this.props.id;
     }
-    const label = (typeof this.props.label === 'undefined' ? '' : this.props.label);
+    const label =
+      typeof this.props.label === "undefined" ? "" : this.props.label;
     return [
-      'frc',
-      this.props.name.split('[').join('_').replace(']', ''),
+      "frc",
+      this.props.name
+        .split("[")
+        .join("_")
+        .replace("]", ""),
       this.hashString(JSON.stringify(label))
-    ].join('-');
+    ].join("-");
   },
-  
-  hasErrors: function () {
+
+  hasErrors: function() {
     return !!(this.props.errors && this.props.errors.length);
   },
-  
-  cleanProps: function (props) {
+
+  cleanProps: function(props) {
     const removedFields = [
-      'beforeComponent',
-      'afterComponent',
-      'addonAfter',
-      'addonBefore',
-      'help',
-      'label',
-      'hideLabel',
-      'options',
-      'layout',
-      'rowLabel',
-      'validatePristine',
-      'validateOnSubmit',
-      'inputClassName',
-      'optional',
-      'throwError',
-      'currentValues',
-      'addToDeletedValues',
-      'deletedValues',
-      'clearFieldErrors',
-      'formType',
-      'inputType',
-      'showCharsRemaining',
-      'charsCount',
-      'charsRemaining',
-      'handleChange',
-      'document',
-      'updateCurrentValues',
-      'classes',
-      'errors',
-      'description',
-      'clearField',
-      'regEx',
-      'mustComplete',
-      'renderComponent',
-      'formInput',
-      'className',
-      'formatValue',
-      'scrubValue',
-      'custom',
-      'hideClear',
-      'inputProperties',
-      'currentUser',
-      'nestedSchema',
-      'parentFieldName',
-      'itemIndex',
-      'formComponents',
+      "beforeComponent",
+      "afterComponent",
+      "addonAfter",
+      "addonBefore",
+      "help",
+      "label",
+      "hideLabel",
+      "options",
+      "layout",
+      "rowLabel",
+      "validatePristine",
+      "validateOnSubmit",
+      "inputClassName",
+      "optional",
+      "throwError",
+      "currentValues",
+      "addToDeletedValues",
+      "deletedValues",
+      "clearFieldErrors",
+      "formType",
+      "inputType",
+      "showCharsRemaining",
+      "charsCount",
+      "charsRemaining",
+      "handleChange",
+      "document",
+      "updateCurrentValues",
+      "classes",
+      "errors",
+      "description",
+      "clearField",
+      "regEx",
+      "mustComplete",
+      "renderComponent",
+      "formInput",
+      "className",
+      "formatValue",
+      "scrubValue",
+      "custom",
+      "hideClear",
+      "inputProperties",
+      "currentUser",
+      "nestedSchema",
+      "parentFieldName",
+      "itemIndex",
+      "formComponents"
     ];
-    
+
     return _omit(props, removedFields);
   },
-  
-  cleanSwitchProps: function (props) {
-    const removedFields = [
-      'value',
-      'error',
-    ];
-  
+
+  cleanSwitchProps: function(props) {
+    const removedFields = ["value", "error"];
+
     return _omit(props, removedFields);
-  },
-  
+  }
 };
